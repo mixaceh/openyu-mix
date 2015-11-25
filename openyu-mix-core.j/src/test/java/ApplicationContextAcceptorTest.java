b85b40c719255243eea18f7704abf1532b2624b0
mixaceh <mixaceh@gmail.com>
2015-11-25 23:47:26 +0800

    fix test error

--- openyu-mix-core.j/src/test/java/ApplicationContextAcceptorTest.java
+++ openyu-mix-core.j/src/test/java/ApplicationContextAcceptorTest.java
@@ -4,70 +4,71 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.openyu.commons.junit.supporter.BaseTestSupporter;
+import org.openyu.commons.thread.ThreadHelper;
 import org.openyu.socklet.acceptor.service.AcceptorService;
 
-public class ApplicationContextAcceptorTest extends BaseTestSupporter
-{
+public class ApplicationContextAcceptorTest extends BaseTestSupporter {
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception
-	{
-		applicationContext = new ClassPathXmlApplicationContext(new String[] {
-				"applicationContext-init.xml",// 
-				"META-INF/applicationContext-commons-core.xml",//				
-				"applicationContext-database.xml",//
-				"applicationContext-database-log.xml",//
-				"applicationContext-schedule.xml",//
-				"META-INF/applicationContext-sls.xml",//
+	public static void setUpBeforeClass() throws Exception {
+		applicationContext = new ClassPathXmlApplicationContext(new String[] { //
+				"applicationContext-init.xml", //
+				"applicationContext-bean.xml", //
 				"applicationContext-acceptor.xml",//
 		});
 	}
 
 	@Test
-	public void accountAcceptor()
-	{
+	public void accountAcceptor() {
 		AcceptorService bean = (AcceptorService) applicationContext.getBean("accountAcceptor");
 		System.out.println(bean);
 		assertNotNull(bean);
+		//
+		ThreadHelper.sleep(60 * 1000);
 	}
 
 	@Test
-	public void loginAcceptor()
-	{
+	public void loginAcceptor() {
 		AcceptorService bean = (AcceptorService) applicationContext.getBean("loginAcceptor");
 		System.out.println(bean);
 		assertNotNull(bean);
+		//
+		ThreadHelper.sleep(5 * 1000);
 	}
 
 	@Test
-	public void masterAcceptor()
-	{
+	public void masterAcceptor() {
 		AcceptorService bean = (AcceptorService) applicationContext.getBean("masterAcceptor");
 		System.out.println(bean);
 		assertNotNull(bean);
+		//
+		ThreadHelper.sleep(5 * 1000);
 	}
 
 	@Test
-	public void slave1Acceptor()
-	{
+	public void slave1Acceptor() {
 		AcceptorService bean = (AcceptorService) applicationContext.getBean("slave1Acceptor");
 		System.out.println(bean);
 		assertNotNull(bean);
+		//
+		ThreadHelper.sleep(10 * 1000);
 	}
 
 	@Test
-	public void slave2Acceptor()
-	{
+	public void slave2Acceptor() {
 		AcceptorService bean = (AcceptorService) applicationContext.getBean("slave2Acceptor");
 		System.out.println(bean);
 		assertNotNull(bean);
+		//
+		ThreadHelper.sleep(5 * 1000);
 	}
 
 	@Test
-	public void slave3Acceptor()
-	{
+	public void slave3Acceptor() {
 		AcceptorService bean = (AcceptorService) applicationContext.getBean("slave3Acceptor");
 		System.out.println(bean);
 		assertNotNull(bean);
+		//
+		ThreadHelper.sleep(5 * 1000);
 	}
 }
--- openyu-mix-core.j/src/test/java/ApplicationContextTest.java
+++ openyu-mix-core.j/src/test/java/ApplicationContextTest.java
@@ -3,15 +3,20 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.openyu.commons.junit.supporter.BaseTestSupporter;
+import org.openyu.commons.lang.RuntimeHelper;
 
 public class ApplicationContextTest extends BaseTestSupporter {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		begTime = System.nanoTime();
-		applicationContext = new ClassPathXmlApplicationContext(
-				new String[] { "applicationContext.xml" });
-		endTime = System.nanoTime();
+		begTime = System.currentTimeMillis();
+		// 計算所耗費的記憶體(bytes)
+		RuntimeHelper.gc();
+		// 原本的記憶體
+		long memory = RuntimeHelper.usedMemory();
+		applicationContext = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
+		endTime = System.currentTimeMillis();
+		usedMemory = Math.max(usedMemory, (RuntimeHelper.usedMemory() - memory));
 	}
 
 	@Test
@@ -21,8 +26,7 @@ public class ApplicationContextTest extends BaseTestSupporter {
 
 	@Test
 	public void getResource() {
-		Resource resource = applicationContext
-				.getResource("applicationContext.xml");
+		Resource resource = applicationContext.getResource("applicationContext.xml");
 		System.out.println(resource + " exist: " + resource.exists());
 		//
 		resource = applicationContext.getResource("WEB-INF/web.xml");
@@ -30,7 +34,12 @@ public class ApplicationContextTest extends BaseTestSupporter {
 	}
 
 	@Test
-	public void showBeans() {
-		printBeans();
+	// count: 49, time: 0 mills., 64,698,592 bytes (64.7 MB) memory used
+	// count: 49, time: 0 mills., 57,141,520 bytes (57.14 MB) memory used
+	// count: 49, time: 0 mills., 59,736,456 bytes (59.74 MB) memory used
+	// count: 57, time: 0 mills., 52,484,168 bytes (52.48 MB) memory used
+	// count: 57, time: 0 mills., 55,889,232 bytes (55.89 MB) memory used
+	public void showBean() {
+		printBean();
 	}
 }
