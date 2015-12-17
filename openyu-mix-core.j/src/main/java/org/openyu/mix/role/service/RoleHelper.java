a359ca891ab31045d0fdc6a27955a96d0bfff1c7
mixaceh <mixaceh@gmail.com>
2015-12-18 00:54:27 +0800

    update final

--- openyu-mix-core.j/src/main/java/org/openyu/mix/role/service/RoleHelper.java
+++ openyu-mix-core.j/src/main/java/org/openyu/mix/role/service/RoleHelper.java
@@ -7,44 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.openyu.mix.flutter.vo.Attribute;
 import org.openyu.mix.flutter.vo.AttributeGroup;
 import org.openyu.mix.flutter.vo.AttributeType;
+import org.openyu.commons.helper.ex.HelperException;
 import org.openyu.commons.helper.supporter.BaseHelperSupporter;
 import org.openyu.socklet.message.vo.Message;
 
-/**
- * The Class RoleHelper.
- */
-public class RoleHelper extends BaseHelperSupporter {
+public final class RoleHelper extends BaseHelperSupporter {
 
-	/** The Constant LOGGER. */
-	private static final transient Logger LOGGER = LoggerFactory
-			.getLogger(RoleHelper.class);
+	private static final transient Logger LOGGER = LoggerFactory.getLogger(RoleHelper.class);
 
-	/**
-	 * Instantiates a new blank helper.
-	 */
 	private RoleHelper() {
-		super();
-		if (InstanceHolder.INSTANCE != null) {
-			throw new UnsupportedOperationException("Can not construct.");
-		}
-	}
-
-	/**
-	 * The Class InstanceHolder.
-	 */
-	private static class InstanceHolder {
-
-		/** The Constant INSTANCE. */
-		private static final RoleHelper INSTANCE = new RoleHelper();
-	}
-
-	/**
-	 * Gets the single instance of BlankHelper.
-	 *
-	 * @return single instance of BlankHelper
-	 */
-	public static RoleHelper getInstance() {
-		return InstanceHolder.INSTANCE;
+		throw new HelperException(
+				new StringBuilder().append(RoleHelper.class.getName()).append(" can not construct").toString());
 	}
 
 	/**
@@ -65,10 +38,8 @@ public class RoleHelper extends BaseHelperSupporter {
 	 * @param message
 	 * @param attributeGroup
 	 */
-	public static void fillAttributeGroup(Message message,
-			AttributeGroup attributeGroup) {
-		Map<AttributeType, Attribute> attributes = attributeGroup
-				.getAttributes();
+	public static void fillAttributeGroup(Message message, AttributeGroup attributeGroup) {
+		Map<AttributeType, Attribute> attributes = attributeGroup.getAttributes();
 		message.addInt(attributes.size());
 		for (Map.Entry<AttributeType, Attribute> entry : attributes.entrySet()) {
 			fillAttribute(message, entry.getValue());
